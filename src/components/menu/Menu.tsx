a27dfa204dbeb76c94dd5ea74e341f0ea5f5cc27
JimRohn <genuaf@lsbu.ac.uk>
2024-02-14 15:21:59 +0000

    second commit : Navbar ready using Map functionality. Grid box template in Homepage using CSS Grid

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,40 +1,44 @@
-import { Link } from "react-router-dom"
-import "./menu.scss"
+import { Link } from "react-router-dom";
+import "./menu.scss";
+import { menu } from "../../data";
 
 export const Menu = () => {
   return (
     <div className="menu">
-         <div className="item">
-        <span className="title">MAIN</span>
-        {/* To link a page in React we use the Link component from 
-        the react-router-dom library. The Link component is used to navigate between pages in a React application.
-        The Link component is similar to the anchor tag in HTML. */}
-      <Link to="/home">
-      <img src="/home.svg" alt="" />
-      <span className="ListItemTitle">HOME</span>
-      </Link>
-      
-      <Link to="/profile">
-      <img src="/profile.svg" alt="" />
-      <span className="ListItemTitle">Profile</span>
-      </Link>
-         </div>
-         <div className="item">
-        <span className="title">MAIN</span>
-        {/* To link a page in React we use the Link component from 
-        the react-router-dom library. The Link component is used to navigate between pages in a React application.
-        The Link component is similar to the anchor tag in HTML. */}
-      <Link to="/home">
-      <img src="/home.svg" alt="" />
-      <span className="ListItemTitle">HOME</span>
-      </Link>
-      
-      <Link to="/profile">
-      <img src="/profile.svg" alt="" />
-      <span className="ListItemTitle">Profile</span>
-      </Link>
-         </div>
-      </div>
-  )
-}
-export default Menu
+      {menu.map((item) => (
+        <div className="item" key={item.id}>
+          <span className="title">{item.title}</span>
+     
+          {item.listItems.map((listItem) => (
+            <Link to="/" className="listItem" key={listItem.id}>
+              <img src={listItem.icon} alt="" />
+              <span className="listItemTitle">{listItem.title}</span>
+            </Link>
+          ))}
+        </div>
+
+        //    {/* //I have to map through the menu array and display the list items that's why
+        //    I have commented out the code below to avoid repetition and long code. */}
+        //    {/* <div className="item">
+        //   <span className="title">MAIN</span>
+
+        // <Link to="/home" className="listItem">
+        // <img src="/home.svg" alt="" />
+        // <span className="listItemTitle">HOME</span>
+        // </Link>
+
+        // <Link to="/" className="listItem">
+        // <img src="/profile.svg" alt="" />
+        // <span className="listItemTitle">Profile</span>
+        // </Link>
+        //    </div> */}
+      ))}
+    </div>
+  );
+};
+export default Menu;
+
+    /* To link a page in React we use the Link component from the
+          react-router-dom library. The Link component is used to navigate
+          between pages in a React application. The Link component is similar to
+          the anchor tag in HTML. */
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,13 @@
-import "./home.scss"
+import "./home.scss";
 
 const Home = () => {
   return (
     <div className="home">
-    Home
-    
+      <div className="box box1">Box1</div>
+      <div className="box box2">Box2</div>
+      <div className="box box3">Box3</div>
+      <div className="box box4">Box4</div>
     </div>
   );
-}
+};
 export default Home;
